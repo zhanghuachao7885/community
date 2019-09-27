260fce1c6c6f1ccd2daa543b981bd8cee4d884f7
zhanghuachao7885 <756921868@qq.com>
2019-09-27 11:30:54 +0800

    test

---
+++ src/main/java/com/practice/sociality/sociality1/Sociality1Application.java
@@ -0,0 +1,13 @@
+package com.practice.sociality.sociality1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sociality1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sociality1Application.class, args);
+    }
+
+}
